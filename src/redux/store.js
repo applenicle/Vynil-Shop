a131a5ac02514910a46fa10facbb9c16d74d600b
Applenicle <gamer31122002@gmail.com>
2022-12-03 15:41:43 +0500

    localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import CartReducer from './CartSlice/slice';
 import CategoryReducer from './CategorySlice/slice';
 import ItemsReducer from './ItemSlice/slice';
+import { fetchItems } from './ItemSlice/asyncAction';
+
+const rootReducer = combineReducers({
+  ItemsReducer,
+  CategoryReducer,
+  CartReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blackList: [fetchItems],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    CartReducer,
-    CategoryReducer,
-    ItemsReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
